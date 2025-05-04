0af7eb1699969e1d85d551bd6b2183a33762cd3b
Sergio Ayerdis <sayerdis@cln1890.com>
2025-05-03 18:54:08 -0600

    nativewind and use of fonts

---
+++ 05-navigation-app/app/_layout.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { Slot, SplashScreen } from "expo-router";
+import "../global.css";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null; // You can also return a loading indicator here
+
+  return <Slot />;
+};
+
+export default RootLayout;
--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -1,11 +1,27 @@
 import { View, Text } from "react-native";
 import React from "react";
+import "../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola mundo</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-5">
+        <Text
+          className="text-5xl"
+          style={{
+            fontFamily: "WorkSans-Black",
+          }}
+        >
+          Hola mundo
+        </Text>
+
+        <Text className="text-4xl font-work-black">Hola mundo</Text>
+        <Text className="text-3xl font-work-light">Hola mundo</Text>
+        <Text className="text-2xl font-work-medium">Hola mundo</Text>
+        <Text className="text-xl">Hola mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
